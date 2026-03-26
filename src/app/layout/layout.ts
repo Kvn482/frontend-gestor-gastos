9012dae401b00eef6cf1d52671ccd1260c23c827
KvnAgromaster <dev1@agromaster.com.mx>
2026-03-26 15:58:33 -0700

    Agregar foto y username al sidebar

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.html',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, RouterLink, RouterLinkActive]
 })
 export class Layout {
 
@@ -14,6 +14,18 @@ export class Layout {
     private authService: AuthService,
     private router: Router
   ) {}
+
+  nombre = ''
+  username = ''
+  profileImageUrl = `https://ui-avatars.com/api/?name=`
+  sidebarOpen = false
+
+  ngOnInit() {
+    const currentUser = this.authService.getCurrentUser();
+    this.nombre = currentUser.nombre.split(" ")[0]
+    this.profileImageUrl = `https://ui-avatars.com/api/?name=${this.nombre}`
+    this.username = currentUser.username
+  }
 
   logout() {
     this.authService.logout();
