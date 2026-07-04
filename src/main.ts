11b891e7bfb9b451f882fbbbfbbdc10310d873d4
Kvn482 <kevinacostaespinoza@gmail.com>
2026-07-03 18:42:07 -0700

    Agregar soporte para PWA: incluir manifest, íconos y service worker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    if ('serviceWorker' in navigator && environment.production) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js');
+      });
+    }
+  })
   .catch((err) => console.error(err));
